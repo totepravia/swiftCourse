73efa278c401ed023c276b0210b631ff971048ab
JOSE GUILLERMO GARCIA BLANCO <joseguillermogarciablanco@mac-mini-de-jose.local>
2018-02-04 20:02:30 +0100

    Initial Commit

---
+++ Tarea 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea 1
+//
+//  Created by JOSE GUILLERMO GARCIA BLANCO on 4/2/18.
+//  Copyright © 2018 JOSE GUILLERMO GARCIA BLANCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
